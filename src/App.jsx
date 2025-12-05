6d5e5aa92b95a837a3f22dc8f01bf5e11b79f382
firas1996 <firas.hm.1996@gmail.com>
2025-12-05 14:20:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Login from "./components/Login";
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <Login />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h2>My name is ......</h2>
+    </>
+  );
+};
+
+export default Login;
