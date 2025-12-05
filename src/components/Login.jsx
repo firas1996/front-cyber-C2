8e864cbb67274893c7d382e82ef4c1df80b26a2c
firas1996 <firas.hm.1996@gmail.com>
2025-12-05 17:02:39 +0100

    fdf

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  const loginHandler = () => {};
+  const [users, setUsers] = useState([]);
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+  const inputChangeHandler = ({ target }) => {
+    const { value, name } = target;
+    setUserData({ ...userData, [name]: value });
+  };
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    const res = await axios.get("http://127.0.0.1:1234/user");
+    // const res = await axios.post("http://127.0.0.1:1234/user/signin", userData);
+    setUsers(res.data.data);
+    console.log(res);
+    setUserData({
+      email: "",
+      password: "",
+    });
+  };
   return (
-    <>
+    <form onSubmit={loginHandler}>
       <label>Email</label>
-      <input type="email" />
+      <input
+        type="email"
+        name="email"
+        value={userData.email}
+        onChange={inputChangeHandler}
+      />
       <label>Password</label>
-      <input type="password" />
-      <button onClick={loginHandler}>Login</button>
-    </>
+      <input
+        type="password"
+        name="password"
+        value={userData.password}
+        onChange={inputChangeHandler}
+      />
+      <button type="submit">Login</button>
+    </form>
   );
 };
 
